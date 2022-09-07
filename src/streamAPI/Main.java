b35caa9310cd712afd3594c2395cfd05e21d10a9
Maks Burenko <Cherniymaks@yandex.ru>
2022-09-07 14:21:30 +0300

    123

--- src/streamAPI/Main.java
+++ src/streamAPI/Main.java
@@ -1,23 +1,45 @@
 package streamAPI;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = null;
-        try {
-            sc = new Scanner(new BufferedReader(new InputStreamReader(System.in, "UTF-8")));
-            sc.useDelimiter("\s");
-            while (sc.hasNext()) {
-                message = sc.nextLine();
+
+        //    try {
+        Scanner sc = new Scanner("Мама мыла раму 33 раза, а рама мыла маму.");
+        // Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in, "UTF-8")));
+        sc.useDelimiter("\\p{Punct}\\s|[^a-zA-ZА-Яа-я0-9]");
+
+        ArrayList<String> list = null;
+        while (sc.hasNext()) {
+            list = new ArrayList<>();
+            list.add((sc.next()).toLowerCase());
+        }
+        sc.close();
+        //    } catch (UnsupportedEncodingException e) {
+        //       e.printStackTrace();
+        //   }
+
+        int i =0;
+//множество с дубликатами
+        HashMap<String, Integer> duplicates = new HashMap<>();
+//множество для отслеживания повторяющихся элементов
+        Set<String> bufer = new HashSet<>();
+//пробегаемся по всем элементам
+        if (list != null) {
+            for (String temp : list) {
+                //добавляем их во множество для отслеживания
+                //если не получилось добавить, то значит элемент уже встречался в списке
+                if (!bufer.add(temp)) {
+                    //в этом случае добавляем его во множество дубликатов
+                    duplicates.put(temp, i++);
+                }
             }
-            sc.close();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            Set <String> keys = duplicates.keySet();
+            System.out.println(keys);
+            Set <Map.Entry<String, Integer>> pair = duplicates.entrySet();
+            System.out.println(pair);
         }
     }
 }
