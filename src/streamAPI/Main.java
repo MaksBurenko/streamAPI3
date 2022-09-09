4ef544b3434b49f81ec8a0e7c09b843934f2880c
Maks Burenko <Cherniymaks@yandex.ru>
2022-09-09 15:00:33 +0300

    123

--- src/streamAPI/Main.java
+++ src/streamAPI/Main.java
@@ -7,15 +7,21 @@ public class Main {
     public static void main(String[] args) {
 
         //    try {
-        Scanner sc = new Scanner("Мама мыла раму 33 раза, а рама мыла маму.");
+        Scanner sc = new Scanner("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                " Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa." +
+                " Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus." +
+                " Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                " Integer vel odio nec mi tempor dignissim.");
+
         // Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in, "UTF-8")));
         sc.useDelimiter("\\p{Punct}\\s|[^a-zA-ZА-Яа-я0-9]");
 
-        ArrayList<String> list = null;
+        ArrayList<String> list = new ArrayList<>();
         while (sc.hasNext()) {
-            list = new ArrayList<>();
             list.add((sc.next()).toLowerCase());
+
         }
+
         sc.close();
         //    } catch (UnsupportedEncodingException e) {
         //       e.printStackTrace();
@@ -27,7 +33,7 @@ public class Main {
 //множество для отслеживания повторяющихся элементов
         Set<String> bufer = new HashSet<>();
 //пробегаемся по всем элементам
-        if (list != null) {
+
             for (String temp : list) {
                 //добавляем их во множество для отслеживания
                 //если не получилось добавить, то значит элемент уже встречался в списке
@@ -36,10 +42,10 @@ public class Main {
                     duplicates.put(temp, i++);
                 }
             }
-            Set <String> keys = duplicates.keySet();
-            System.out.println(keys);
-            Set <Map.Entry<String, Integer>> pair = duplicates.entrySet();
-            System.out.println(pair);
-        }
+            duplicates.entrySet().stream()
+                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                    .map(Map.Entry::getKey)
+                    .limit(10)
+                    .forEach(System.out::println);
     }
 }
