c12378999bb327434285792e82ffae1bc2be3331
Maks Burenko <Cherniymaks@yandex.ru>
2022-09-12 15:01:36 +0300

    123

--- src/streamAPI/Main.java
+++ src/streamAPI/Main.java
@@ -1,39 +1,27 @@
 package streamAPI;
-
 import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        //    try {
-        Scanner sc = new Scanner("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
-                " Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa." +
-                " Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus." +
-                " Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
-                " Integer vel odio nec mi tempor dignissim.");
+        Scanner sc = new Scanner("Мама мыла-мыла-мыла раму!");
 
-        // Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in, "UTF-8")));
+        // Scanner sc = new Scanner(System.in, "UTF-8");
         sc.useDelimiter("\\p{Punct}\\s|[^a-zA-ZА-Яа-я0-9]");
 
         ArrayList<String> list = new ArrayList<>();
         while (sc.hasNext()) {
             list.add((sc.next()).toLowerCase());
-
         }
-
         sc.close();
-        //    } catch (UnsupportedEncodingException e) {
-        //       e.printStackTrace();
-        //   }
 
         int i =0;
 //множество с дубликатами
         HashMap<String, Integer> duplicates = new HashMap<>();
 //множество для отслеживания повторяющихся элементов
         Set<String> bufer = new HashSet<>();
-//пробегаемся по всем элементам
 
+//пробегаемся по всем элементам
             for (String temp : list) {
                 //добавляем их во множество для отслеживания
                 //если не получилось добавить, то значит элемент уже встречался в списке
@@ -46,6 +34,23 @@ public class Main {
                     .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
                     .map(Map.Entry::getKey)
                     .limit(10)
+                    .forEach(System.out::println);
+
+            //перебор буфера и удаление совпадений с дубликатами, на случай если дубликатов окажется меньше 10
+            if (duplicates.size() < 10) {
+                Set<String> keys = duplicates.keySet();
+                for (String temp : bufer) {
+                    for (String key : keys) {
+                        if (temp.equals(key)){
+                            bufer.remove(temp);
+                        }
+                    }
+                }
+            }
+            //сортировка в алфавитном порядке (по дефолту) и добивание до лимита 10
+            bufer.stream()
+                    .sorted()
+                    .limit(10 - duplicates.size())
                     .forEach(System.out::println);
     }
 }
