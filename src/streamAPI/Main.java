f0d755624ef00ebb311fb91f5a9dc573aa6253de
Maks Burenko <Cherniymaks@yandex.ru>
2022-09-20 15:18:02 +0300

    123

--- src/streamAPI/Main.java
+++ src/streamAPI/Main.java
@@ -1,13 +1,12 @@
 package streamAPI;
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner("Мама мыла-мыла-мыла раму!");
+        Scanner sc = new Scanner("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.");
 
         // Scanner sc = new Scanner(System.in, "UTF-8");
         sc.useDelimiter("\\p{Punct}\\s|[^a-zA-ZА-Яа-я0-9]");
@@ -18,53 +17,16 @@ public class Main {
         }
         sc.close();
 
-        int i =0;
+
         Map<String, Integer> duplicates = list.stream()
-                        .collect(Collectors.toMap(x1 -> x1, (x1, x2) -> x1, i -> i++));
+                .collect(Collectors.toMap(x -> x, i -> 1, (oldValue,newValue) -> newValue = oldValue + 1));
 
 
-/*
-        int i =0;
-//множество с дубликатами
-        HashMap<String, Integer> duplicates = new HashMap<>();
-//множество для отслеживания повторяющихся элементов
-        Set<String> bufer = new HashSet<>();
+        duplicates.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
+                .map(Map.Entry::getKey)
+                .limit(10)
+                .forEach(System.out::println);
 
-//пробегаемся по всем элементам
-            for (String temp : list) {
-                //добавляем их во множество для отслеживания
-                //если не получилось добавить, то значит элемент уже встречался в списке
-                if (!bufer.add(temp)) {
-                    //в этом случае добавляем его во множество дубликатов
-                    duplicates.put(temp, i++);
-                }
-            }*/
-            duplicates.entrySet().stream()
-                    .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                    .map(Map.Entry::getKey)
-                    .limit(10)
-                    .forEach(System.out::println);
-
-
-            //перебор буфера и удаление совпадений с дубликатами, на случай если дубликатов окажется меньше 10
-
-            if (duplicates.size() < 10) {
-
-                Iterator<String> iteratorDuplicates = duplicates.keySet().iterator();
-                Iterator<String> iteratorBufer = bufer.iterator();
-
-                while (iteratorDuplicates.hasNext()) {
-                    while (iteratorBufer.hasNext()) {
-                        if (iteratorDuplicates.next().equals(iteratorBufer.next())){
-                            iteratorBufer.remove();
-                        }
-                    }
-                }
-            }
-            //сортировка в алфавитном порядке (по дефолту) и добивание до лимита 10
-            bufer.stream()
-                    .sorted()
-                    .limit(10 - duplicates.size())
-                    .forEach(System.out::println);
     }
 }
